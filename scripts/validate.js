fb89d9e93b38d4abf82254b6cffdb310cf9910d2
abroksana <96975788+abroksana@users.noreply.github.com>
2022-04-18 11:31:13 +0300

    Проектная 6; разбиение кода валидации на функции

--- scripts/validate.js
+++ scripts/validate.js
@@ -22,10 +22,25 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся функция
+    // hasInvalidInput вернёт true
+
+    return !inputElement.validity.valid;
+  })
+};
+
 const setEventListeners = (formElement) => {
   // Находим все поля внутри формы,
   // сделаем из них массив методом Array.from
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const inputList = Array.from(formElement.querySelectorAll(`.${classData.inputSelector}`));
+  const buttonElement = formElement.querySelector(`.${classData.buttonSubmitClass}`);
+
+  // чтобы проверить состояние кнопки в самом начале
+  toggleButtonState(inputList, buttonElement, classData.disableButtonClass);
 
   // Обойдём все элементы полученной коллекции
   inputList.forEach((inputElement) => {
@@ -34,65 +49,48 @@ const setEventListeners = (formElement) => {
       // Внутри колбэка вызовем isValid,
       // передав ей форму и проверяемый элемент
       checkInputValidity(formElement, inputElement);
+      // чтобы проверять его при изменении любого из полей
+      toggleButtonState(inputList, buttonElement, classData.disableButtonClass);
     });
   });
 };
 
-const enableValidation = () => {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll('.popup__container'));
+const toggleButtonState = (inputList, buttonElement, disableButtonClass) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.classList.add(disableButtonClass);
+  } else {
+    // иначе сделай кнопку активной
+    buttonElement.classList.remove(disableButtonClass);
+  }
+};
 
-  // Переберём полученную коллекцию
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__container'));
   formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      // У каждой формы отменим стандартное поведение
+    formElement.addEventListener('submit', function (evt) {
       evt.preventDefault();
     });
-
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
-    setEventListeners(formElement);
+  const fieldsetList = Array.from(formElement.querySelectorAll('.popup__fieldset'));
+  fieldsetList.forEach((fieldSet) => {
+    setEventListeners(fieldSet);
   });
+  });
+};
+
+
+const classData = {
+  inputSelector: 'popup__input',
+  buttonSubmitClass: 'popup__submit-button',
+  disableButtonClass: 'popup__submit-button_inactive'
+  // inputErrorClass: 'popup__input_type_error',
+  // activeErrorClass: 'popup__input-error_visible'
 };
 
 // Вызовем функцию
-enableValidation();
+enableValidation(classData);
 
-/*
-// const form = popupPrflFrm.querySelector('.popup__container');
-const formInput = form.querySelector('.popup__input');
-const formError = form.querySelector(`#${formInput.id}-error`); // от id к id
-// console.log(formInput.id);
-
-
-setEventListeners(form );
-
-
-
-
-
-form.addEventListener('submit', function (evt) {
-evt.preventDefault();
-});
-
-formInput.addEventListener('input', function () {
-checkInputValidity();
-});
-
-*/
-
-// включение валидации вызовом enableValidation
-// все настройки передаются при вызове
-
-// enableValidation({
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-
-//   submitButtonSelector: '.popup__button',
-//   inactiveButtonClass: 'popup__button_disabled',
-
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible'
-// });
+// const disableButtonClass = document.querySelector('.popup__submit-button_inactive');
 
